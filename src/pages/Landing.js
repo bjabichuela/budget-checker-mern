07d5ee9eedbdace516335dc02d2032f0ab0f20e8
bjabichuela <bjabichuela@gmail.com>
2021-11-26 08:48:55 +0800

    Added hero for landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,17 +1,11 @@
 import React from 'react'
-import { Row, Col, Container} from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import '../index.css'
-import Budget from '../images/budgetchecker.jpg'
+
 
 const Landing = () => {
   return (
-    <Container className="py-2 pl-0 ml-0" fluid>
-        <Row className="justify-content-center">
-            <Col sm={12} md={4}>
-                <img className="responsive" src={Budget} alt='finance' width='520px' height='500px'/>
-            </Col>
-        </Row>
-    </Container>
+    <Container className="hero" fluid></Container>
   )
 }
 
